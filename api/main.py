2063ab495bb9fc113ce68584174b2fd138a9a6ea
kdadobe <134492419+kdadobe@users.noreply.github.com>
2025-02-22 22:08:45 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -16,20 +16,23 @@ class TextRequest(BaseModel):
 
 # Define response model for structured JSON output
 class BiasResponse(BaseModel):
+    complete_statement: str
+    initial_bias_score: float
+    post_processed_statement: str
     final_statement: str
-    bias_score: float  # Assuming the score is a float
+    final_score: float
 
 @app.post("/unbias", response_model=BiasResponse)
 async def process_statement(request: TextRequest = Body(...)):
     """API endpoint to detect bias in text via POST request body"""
-    result, score = bias_filter.process_statement(request.text)
-    return BiasResponse(final_statement=result, bias_score=score)
+    r1,r2,r3,r4,r5 = bias_filter.process_statement(request.text)
+    return BiasResponse(complete_statement=r1, initial_bias_score=r2, post_processed_statement=r3, final_statement=r4, final_score=r5)
 
 @app.get("/unbias", response_model=BiasResponse)
 async def process_statement_get(text: str = Query(..., description="Text to be processed")):
     """API endpoint to detect bias in text via GET query parameter"""
-    result, score = bias_filter.process_statement(text)
-    return BiasResponse(final_statement=result, bias_score=score)
+    r1,r2,r3,r4,r5 = bias_filter.process_statement(text)
+    return BiasResponse(complete_statement=r1, initial_bias_score=r2, post_processed_statement=r3, final_statement=r4, final_score=r5)
 
 @app.get("/")
 async def root():
@@ -40,17 +43,24 @@ async def root():
 
 # Define a function for Gradio UI
 def gradio_unbias(text: str):
-    result, score = bias_filter.process_statement(text)
-    return result, score
+    r1,r2,r3,r4,r5 = bias_filter.process_statement(text)
+    return r1,r2,r3,r4,r5
 
 # Create Gradio interface
 gradio_app = gr.Interface(
     fn=gradio_unbias, 
-    inputs=gr.Textbox(label="Enter a masked statement with [MASK] for BERT to predict the word"),
-    outputs=[gr.Textbox(label="Final Statement"), gr.Number(label="Bias Score")], 
+    inputs=gr.Textbox(label="Enter a masked statement with [MASK] to predict the word by BERT model"),  # Input with a label
+    outputs=[
+        gr.Textbox(label="Sentence after BERT predicted the word for [MASK]"), 
+        gr.Number(label="Initial Bias Score"), 
+        gr.Textbox(label="Sentence after post processing filters are applied"), 
+        gr.Textbox(label="Final Sentence after Google Flan-t5 processing"), 
+        gr.Number(label="Final Bias Score")
+    ], 
     title="Bias Detection",
-    description="Enter a statement to analyze its bias score."
+    description="Enter a statement to analyze its bias and get a gender-neutral version."
 )
+
 
 # Mount Gradio inside FastAPI at `/gradio`
 app = gr.mount_gradio_app(app, gradio_app, path="/gradio")
